eb5ae97b585f19c49651a547022019f7d8e0e7e7
a-mccluskey <46331308+a-mccluskey@users.noreply.github.com>
2020-04-23 18:24:38 +0100

    Create Progam.cs
    Simple script to call the site every two minutes

---
+++ Progam.cs
@@ -0,0 +1,32 @@
+using System;
+using System.IO;
+using System.Net;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "DNS updater";
+            Console.WriteLine("Started at: " + DateTime.UtcNow + " UTC\nChecking every 2 minutes.\nUse Ctrl+C to exit");
+            while (true)
+            {
+                WebClient client = new WebClient();
+                client.Headers.Add(HttpRequestHeader.UserAgent, "AW DNS IP update check");
+                Stream stream = client.OpenRead("https://www.SiteDomain.com/updateIP.php");
+                StreamReader reader = new StreamReader(stream);
+                String content = reader.ReadToEnd();
+                if(content.Contains("Addresses match"))
+                {
+                    //pass
+                }
+                else
+                {
+                    Console.WriteLine($"Error at [{DateTime.Now}]: {content}");
+                }
+                System.Threading.Thread.Sleep(120000);
+            }
+        }
+    }
+}
